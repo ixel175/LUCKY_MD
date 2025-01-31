110a7f24b510394d248688c92fa26ac76126723a
ixel175 <haferereelijahmpenzi@gmail.com>
2025-01-31 11:54:35 +0300

    Update set.js

--- set.js
+++ set.js
@@ -10,7 +10,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "Fredi Ezra",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " 255752593977",              
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " 256782844339",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'LUCKY_MD',
